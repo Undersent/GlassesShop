6640d293e655d630f3ca52e65492e5eed6744bce
Undersent <rafallawro@gmail.com>
2017-10-23 18:32:08 +0200

    Initial commit

---
+++ src/main/java/com/example/Glasses/GlassesApplication.java
@@ -0,0 +1,12 @@
+package com.example.Glasses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlassesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlassesApplication.class, args);
+	}
+}
